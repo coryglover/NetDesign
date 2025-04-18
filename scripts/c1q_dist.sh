9b60cc74f1bd731cc4dd6c8964e80b70df922968
Cory Glover <glover.co@northeastern.edu>
2025-04-17 22:06:44 -0400

    execute assembly tree

---
+++ scripts/c1q_dist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100GB
+#SBATCH --partition=netsi_standard
+#SBATCH --time=4:00:00
+#SBATCH --job-name=C1Q
+#SBATCH --output=/scratch/glover.co/NetDesign/out/c1q.out
+#SBATCH --error=/scratch/glover.co/NetDesign/c1q.err
+
+python execute_assembly.py
