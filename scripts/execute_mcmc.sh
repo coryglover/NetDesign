0af7c5b57f844e89ef543f2f9f9c226cf1ee86b0
coryglover <glover.co@northeastern.edu>
2025-05-30 21:15:30 -0400

    scripts for running mcmc on cluster

--- scripts/execute_mcmc.sh
+++ scripts/execute_mcmc.sh
@@ -6,13 +6,13 @@
 #SBATCH --time=24:00:00
 #SBATCH --job-name=mcmc_simulation
 #SBATCH --partition=short
-#SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_output.log
-#SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_error.log
-#SBATCH --array=1%100
+#SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_%A_%a.log
+#SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_%A_%a.log
+#SBATCH --array=1
 
 # Read in parameters file
-PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /scratch/glover.co/NetDesign/params/mcmc_params.txt)
+PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /projects/ccnr/glover.co/net_design/NetDesign/params/mcmc_params.txt)
 
-echo $PARAMS
+echo "${PARAMS}"
 # Run mcmc script with parameters
 python /projects/ccnr/glover.co/net_design/NetDesign/scripts/run_mcmc.py $PARAMS
