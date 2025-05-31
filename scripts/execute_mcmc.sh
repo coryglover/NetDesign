8181cb6f675788f082f8d0cd9be19efbea738dd5
coryglover <glover.co@northeastern.edu>
2025-05-30 21:06:21 -0400

    params file edits

--- scripts/execute_mcmc.sh
+++ scripts/execute_mcmc.sh
@@ -6,12 +6,13 @@
 #SBATCH --time=24:00:00
 #SBATCH --job-name=mcmc_simulation
 #SBATCH --partition=short
-#SBATCH --output=/scratch/glover.co/NetDesign/err/mcmc_output.log
-#SBATCH --error=/scratch/glover.co/NetDesign/out/mcmc_error.log
-#SBATCH --1-1%100
+#SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_output.log
+#SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_error.log
+#SBATCH --array=1%100
 
 # Read in parameters file
 PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /scratch/glover.co/NetDesign/params/mcmc_params.txt)
 
+echo $PARAMS
 # Run mcmc script with parameters
-python /scratch/glover.co/NetDesign/scripts/run_mcmc.py $PARAMS
+python /projects/ccnr/glover.co/net_design/NetDesign/scripts/run_mcmc.py $PARAMS
