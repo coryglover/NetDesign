3abad098a0bd114defb69d2ad0e74c9f188b3da5
coryglover <glover.co@northeastern.edu>
2025-06-19 22:02:05 -0400

    changes

--- scripts/execute_mcmc.sh
+++ scripts/execute_mcmc.sh
@@ -4,14 +4,14 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100GB
 #SBATCH --time=24:00:00
-#SBATCH --job-name=Protein_simulation
+#SBATCH --job-name=Prot1_simulation
 #SBATCH --partition=short
 #SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_%A_%a.log
 #SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_%A_%a.log
 #SBATCH --array=1-1000%10
 
 # Read in parameters file
-PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /projects/ccnr/glover.co/net_design/NetDesign/params/proteins/mcmc_params.txt)
+PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /projects/ccnr/glover.co/net_design/NetDesign/params/proteins/mcmc_params_1.txt)
 
 echo "${PARAMS}"
 # Run mcmc script with parameters
