6a3c908772d377fea185522c913a95b02e723332
coryglover <glover.co@northeastern.edu>
2025-08-07 15:38:38 -0400

    mcmc_explorer

--- scripts/execute_mcmc.sh
+++ scripts/execute_mcmc.sh
@@ -2,19 +2,23 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=100GB
-#SBATCH --time=5-00:00:00
+#SBATCH --mem=4GB
+#SBATCH --time=1-00:00:00
 #SBATCH --job-name=Prot1
-#SBATCH --partition=netsi_standard
+#SBATCH --partition=short
 #SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_%A_%a.log
 #SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_%A_%a.log
-#SBATCH --array=1-20%5
+#SBATCH --array=1-1000%5
 
 set -x
 
 echo "hello from bash"
 
-trap 'echo "Caught SIGTERM at $(date)"' TERM
+echo "Job ran on node ${HOSTNAME}"
+
+sleep 1
+
+trap 'echo "Caught SIGTERM at $(date)" >> "$logfile"' TERM 
 trap 'echo "Caught SIGINT at $(date)"' INT
 trap 'echo "Exited with code $?"' EXIT
  
--- scripts/temp_execute_mcmc.sh
+++ scripts/temp_execute_mcmc.sh
@@ -4,9 +4,22 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=100GB
 #SBATCH --time=5-00:00:00
-#SBATCH --job-name=Ikea1
+#SBATCH --job-name=Rob1
 #SBATCH --partition=netsi_standard
 #SBATCH --output=/scratch/glover.co/NetDesign/out/mcmc_%A_%a.log
 #SBATCH --error=/scratch/glover.co/NetDesign/err/mcmc_%A_%a.log
 
+params_file="/projects/ccnr/glover.co/net_design/NetDesign/params/robots/mcmc_params.txt"
 
+count=3
+max_count=10
+
+while IFS= read -r line; do
+	echo "Running with parameters: $line"
+	python /projects/ccnr/glover.co/net_design/NetDesign/scripts/run_mcmc.py $line
+
+	((count++))
+        if [[ $count -ge $max_count ]]; then
+            break
+        fi
+done < $params_file
