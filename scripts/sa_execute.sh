69f64969bf1f4507a7accc17e0f3abe30386af81
Cory Glover <glover.co@northeastern.edu>
2025-08-25 05:27:03 -0400

    selfassembly script

---
+++ scripts/sa_execute.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4GB
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=DF_cir
+#SBATCH --partition=long
+#SBATCH --output=/scratch/glover.co/NetDesign/out/df_%A_%a.log
+#SBATCH --error=/scratch/glover.co/NetDesign/err/df_%A_%a.log
+SBATCH --array=1-886%1 
+
+set -x
+
+echo "hello from bash"
+
+echo "Job ran on node ${HOSTNAME}"
+
+sleep 1
+
+trap 'echo "Caught SIGTERM at $(date)" >> "$logfile"' TERM 
+trap 'echo "Caught SIGINT at $(date)"' INT
+trap 'echo "Exited with code $?"' EXIT
+ 
+# Read in parameters file
+# PARAMS=$(awk "NR==${SLURM_ARRAY_TASK_ID}" /projects/ccnr/glover.co/net_design/NetDesign/params/circuits/analysis_params.txt)
+
+
+# sleep 1
+# echo "${PARAMS}"
+# sleep 1
+# echo "${SLURM_ARRAY_TASK_ID}"
+# Run mcmc script with parameters
+# set -- $PARAMS
+python /projects/ccnr/glover.co/net_design/NetDesign/scripts/self_assembly.py --param_file /projects/ccnr/glover.co/net_design/NetDesign/params/circuits/analysis_params.txt --output /scratch/glover.co/NetDesign/data/circuits/stats/self_assembly.txt
+#python /projects/ccnr/glover.co/net_design/NetDesign/scripts/max_diversity.py
+# sleep 1
+# echo "job ${SLURM_ARRAY_TASK_ID} complete"
---
+++ scripts/self_assembly.py
@@ -0,0 +1,59 @@
+import numpy as np 
+import networkx as nx
+import os
+import pandas as pd 
+import assembly_tree as at 
+import mcmc
+import argparse
+import json 
+import copy
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Analyze graph data and compute assembly trees.')
+    parser.add_argument('--param_file', type=str, required=True, help='Directory containing input files.')
+    parser.add_argument('--output', type=str, required=True, help='File to save results.')
+    parser.add_argument("--c_exclusive",action="store_true",default=False)
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    # Get graphs
+    with open(args.param_file, 'r') as f:
+        lines = f.readlines()
+    # for line in lines:
+    #     line.split(' ')
+    graph_files = [line.split('--graph_file ')[1].split(' --X_file')[0] for line in lines]
+    X_files = [line.split('--X_file ')[1].split(' --')[0] for line in lines]
+    O_file = [line.split('--O_file ')[1].split(' --')[0] if '--O_file' in line else None for line in lines]
+    c_file = [line.split('--c_file ')[1].split(' --')[0] if '--c_file' in line else None for line in lines]
+
+    sa_p = np.zeros(len(graph_files))
+    for i in range(len(graph_files)):
+
+        # Load graph
+        target = nx.read_edgelist(graph_files[i], nodetype=int, create_using=nx.Graph)
+        print(f"Loaded graph with {target.number_of_nodes()} nodes and {target.number_of_edges()} edges.")
+        # Load labels
+        X = np.loadtxt(X_files[i], dtype=int)
+        if X.ndim == 1:
+            X = X.reshape(len(X),1)
+        # Get capacity vector
+        capacity = at.extract_deg_cap(target, X).reshape(-1)
+        if args.c_file is not None:
+            capacity = np.loadtxt(c_file[i])
+        else:
+            capacity = at.extract_deg_cap(target, X).reshape(-1)
+        if args.O_file is not None:
+            O = np.loadtxt(O_file[i])
+        else:
+            if args.c_exclusive:
+                O = (np.ones((X.shape[1],X.shape[1])) * capacity).T
+            else:
+                O = at.extract_O(target, X)
+
+        initial_graph = nx.Graph()
+        initial_graph.add_nodes_from(np.arange(X.shape[0]))
+        sa_p[i] = at.self_assembly(X, O, capacity, initial_graph)
+    
+    np.savetxt(args.output, sa_p)
+
